70ca3f6cbbc4c74f91b77d8ec481b009c163f014
Jere Virta <jere.virta@tahtogroup.fi>
2022-09-25 15:51:19 +0300

    foo

--- nodejs/6nodemoeviedb/index.js
+++ nodejs/6nodemoeviedb/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const query = require('./db/movies');
 
 const app = express();
 app.use(bodyParser.json());
 
 const port = 3000;
+
+
+
+// Get all movies
+app.get("/api/movies", query.getAllMovies)
+
+
+
+
+
+ // Get movie by id
+app.get("/api/movies/:id", query.getMovieById)
+   
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
